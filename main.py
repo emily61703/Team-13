1c367f4201021b52c26ca85eea43eb780a6f6fbd
Aaron Tran <at54973@gmail.com>
2025-11-09 21:23:01 -0600

    sfx import and comment out corrupted wav files

--- main.py
+++ main.py
@@ -4,10 +4,11 @@
 from tkinter import Tk, Label
 from PIL import Image, ImageTk
 from login_screen import show_login_screen
+from sfx_system import soundsystem
 
 # Config
-WINDOW_WIDTH = 500
-WINDOW_HEIGHT = 500
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 800
 SPLASH_DURATION = 3000
 LOGO_SCALE = 0.75  # means logo is 50% it's original size
 
@@ -17,6 +18,10 @@ def main():
     splash_window.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
     splash_window.title("PHOTON - The Ultimate Game on Planet Earth")
     splash_window.configure(bg="black")
+    
+    # play startup sound (Disabled because sound files are corrupted)
+    #soundsystem.play_game_sound('start')
+    
 
     # Open the image file
     image = Image.open("assets/logo.jpg")
@@ -36,6 +41,9 @@ def main():
     splash_label.place(relx=0.5, rely=0.5, anchor="center")
 
     def transition_to_login():
+        # Play exit sound and transition to login screen
+        # soundsystem.play_game_sound('exit') - Disabled
+        # soundsystem.cleanup() - Disabled
         splash_window.destroy()
         show_login_screen()
 
